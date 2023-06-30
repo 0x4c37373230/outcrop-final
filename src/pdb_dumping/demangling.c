2704c59e07924cd281982883368df9fb125f0943
Luke7720 <81709312+0x4c37373230@users.noreply.github.com>
2023-06-29 23:22:11 -0300

    Forgot a fucking file

---
+++ src/pdb_dumping/demangling.c
@@ -0,0 +1,12 @@
+#pragma comment(lib, "DbgHelp.lib")
+
+#include "Windows.h"
+#include "DbgHelp.h"
+
+const char* demangle(const char* symbol)
+{
+    char* buffer = malloc(1024 * sizeof(char));
+    memset(buffer, 0, 1024);
+    UnDecorateSymbolName(symbol, buffer, 1024, 0);
+    return buffer;
+}
